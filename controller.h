42d79b487bbbbac00eea2525320499b5cbd066da
Patrick Hisni Brataas <patrick@brataas.io>
2019-03-23 13:20:56 +0100

    removed unnecessary code

--- controller.h
+++ controller.h
@@ -3,18 +3,16 @@
 
 #include <QIODevice>
 
-class Controller : public QObject {
-    Q_OBJECT
-
+class Controller {
 public:
     void setDevice(QIODevice *device) {
         m_device = device;
     }
 
-public slots:
     void writeRight() {
         m_device->write("Right");
     }
+
     void writeLeft() {
         m_device->write("Left");
     }
